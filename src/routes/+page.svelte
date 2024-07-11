7f743c3b0654e66197bd07b29ac12e83bd94a97a
Arin Salabsi <69157252+arrrin@users.noreply.github.com>
2024-07-11 10:44:13 +0700

    config tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<h1 class="text-3xl font-bold underline">Overtime calculator</h1>
+</div>
