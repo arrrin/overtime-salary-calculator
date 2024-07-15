a6bc4e75b670a2cfe41ce9dd88363f5b6e0b9b7e
Arin Salabsi <69157252+arrrin@users.noreply.github.com>
2024-07-15 08:58:25 +0700

    add form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-  import Header from "../component/header.svelte";
-  import Button from "../component/button.svelte";
+	import Header from '../component/header.svelte';
+	import Button from '../component/button.svelte';
+	import Form from '../component/form.svelte';
 </script>
 
-<div class="bg-gray-700 rounded-b-lg">
-  <Header></Header>
-
-</div>
+<header class="bg-gray-700 rounded-b-lg">
+	<Header></Header>
+</header>
+<body class="bg-gray-100"> <Form></Form></body>
 
